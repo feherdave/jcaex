e6554068a47cd192973c03dfd4793579e7055c0c
feherdave <feherdave@gmail.com>
2023-01-04 13:52:07 +0100

    Fixed module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module org.fd.jcaex {
     requires com.sun.xml.bind;
 
     exports org.fd.jcaex;
+    exports org.fd.jcaex.tools;
     exports org.fd.jcaex.v2_15;
     exports org.fd.jcaex.v3_0;
 }
